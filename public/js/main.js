361e23199ec776d7bc4516c40d9a4a8e4074ebfd
Bheru Singh <rbherusingh440@gmail.com>
2026-06-06 21:51:58 +0530

    animations added

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+// Navbar scroll shadow
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 10) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
